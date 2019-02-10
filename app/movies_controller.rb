d0a94b9633038274c9ea8596ab82992f5b53c87f
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 06:43:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,8 @@ end
 
 def can_be_created_in_a_block(arg)
   Movie.create do |m|
-    m.title="Home Alone"
-    m.release_date=1990
+    m.title = "Home Alone"
+    m.release_date = 1990
   end
 end
 
