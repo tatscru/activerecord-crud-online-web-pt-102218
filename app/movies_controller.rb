3fcba359e83468c4291f94dcec8c50e706a9ca46
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 06:31:02 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   Movie.create do |m|
     m.title = "Home Alone"
