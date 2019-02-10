a116ee76d2981b9853abd224b860d358910f7881
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 06:31:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = nil)
   # If no arguments are passed, use default values:
   Movie.create do |m|
     m.title = "The Room"
-    m.release_date = 1990
+    # m.release_date = 1990
   end
 end
 
