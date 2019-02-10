7aee9152ac19c60885cedf56c7ae355828bd9173
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 07:18:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block()
+def can_be_created_in_a_block({title: "Home Alone", release_date: 1990})
   # Movie. can_be_created_with_a_hash_of_attributes
   Movie.create do |m|
     m.title = "Home Alone"
