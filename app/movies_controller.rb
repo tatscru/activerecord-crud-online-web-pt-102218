7493cfeee4a7244d0ce3bc8a5cf76ec818484eff
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 06:30:31 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -55,6 +55,7 @@ def can_find_by_multiple_attributes
   # release_date == 2000
   # director == "Me"
   Movie.find_by(title: "Title", release_date: 2000, director: "Me")
+  Movie.find_by(attributes)
 end
 
 def can_find_using_where_clause_and_be_sorted
