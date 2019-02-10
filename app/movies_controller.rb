c8522f0b740310c5a97a1e6eb7005f92083ad89b
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 06:33:43 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -14,9 +14,9 @@ end
 def can_be_created_with_a_hash_of_attributes
   # Initialize movie and then and save it
   attributes = {
-      # title: "The Sting",
-      # release_date: 1973,
-      # director: "George Roy Hill",
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
       lead: "Paul Newman",
       in_theaters: false
   }
